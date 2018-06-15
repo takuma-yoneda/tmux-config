896270e5e97b174192ce7bd84e0c6992700482c9
Takuma Yoneda <yoneda@YoneBook-Pro.local>
2018-06-16 00:16:50 +0100

    tiny fix

--- installer-for-docker.sh
+++ installer-for-docker.sh
@@ -6,7 +6,7 @@ ln -s $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
 cd ~/.tmux && git submodule init && git submodule update
 cd ~/.tmux/vendor/tmux-mem-cpu-load && cmake . && make && make install
 # run tmux new -d if tmux daemon is not running
-if [[ $(tmux ls -A) ]]; then
+if [[ $(tmux ls) ]]; then
     tmux new -d
 fi
 tmux source-file ~/.tmux.conf
