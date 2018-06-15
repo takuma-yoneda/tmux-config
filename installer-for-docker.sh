46807b00b4b4297e0ce690e8303ad6f689f6ce42
Takuma Yoneda <yoneda@YoneBook-Pro.local>
2018-06-16 00:01:11 +0100

    remove sudo and make it executable

--- installer-for-docker.sh
+++ installer-for-docker.sh
@@ -4,5 +4,5 @@ trap exit ERR
 
 ln -s $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
 cd ~/.tmux && git submodule init && git submodule update
-cd ~/.tmux/vendor/tmux-mem-cpu-load && cmake . && make && sudo make install
+cd ~/.tmux/vendor/tmux-mem-cpu-load && cmake . && make && make install
 tmux source-file ~/.tmux.conf
