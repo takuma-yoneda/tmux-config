93702767e1fbf90fc17b2e5c90d5c9e223743975
Takuma Yoneda <yoneda@YoneBook-Pro.local>
2018-06-16 00:52:15 +0100

    substitute building part with pip install

--- installer-for-docker.sh
+++ installer-for-docker.sh
@@ -4,7 +4,14 @@ trap exit ERR
 
 ln -s $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
 cd ~/.tmux && git submodule init && git submodule update
-cd ~/.tmux/vendor/tmux-mem-cpu-load && cmake . && make && make install
+
+# pip install instead of building it from scrach.
+pip install psutil
+
+# place it where registered in PATH
+cp ~/.tmux/vendor/basic-cpu-and-memory.tmux /usr/local/bin/tmux-mem-cpu-load
+chmod +x /usr/local/bin/tmux-mem-cpu-load
+
 # run tmux new -d if tmux daemon is not running
 if [[ $(tmux ls) ]]; then
     tmux new -d
