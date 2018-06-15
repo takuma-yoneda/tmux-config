1bbf81d94943dbdcebfaa6899b41f2edcc35503a
Takuma Yoneda <yoneda@YoneBook-Pro.local>
2018-06-16 00:11:13 +0100

    run daemon if it is not running

--- installer-for-docker.sh
+++ installer-for-docker.sh
@@ -5,4 +5,8 @@ trap exit ERR
 ln -s $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
 cd ~/.tmux && git submodule init && git submodule update
 cd ~/.tmux/vendor/tmux-mem-cpu-load && cmake . && make && make install
+# run tmux new -d if tmux daemon is not running
+if [[ $(tmux ls -A) ]]; then
+    tmux new -d
+fi
 tmux source-file ~/.tmux.conf
