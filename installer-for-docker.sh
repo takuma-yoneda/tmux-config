2ada53b898350d2859b899044f811181c400087d
Takuma Yoneda <yoneda@YoneBook-Pro.local>
2018-06-15 23:42:42 +0100

    add install script for docker

---
+++ installer-for-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+trap exit ERR
+
+ln -s $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
+cd ~/.tmux && git submodule init && git submodule update
+cd ~/.tmux/vendor/tmux-mem-cpu-load && cmake . && make && sudo make install
+tmux source-file ~/.tmux.conf
