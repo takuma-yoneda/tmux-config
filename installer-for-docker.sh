349e5ed167a1c4789e7999325cd1a7acdfb27cb1
Takuma Yoneda <yoneda@YoneBook-Pro.local>
2018-06-16 00:58:12 +0100

    run tmux daemon anyway

--- installer-for-docker.sh
+++ installer-for-docker.sh
@@ -12,8 +12,6 @@ pip install psutil
 cp ~/.tmux/vendor/basic-cpu-and-memory.tmux /usr/local/bin/tmux-mem-cpu-load
 chmod +x /usr/local/bin/tmux-mem-cpu-load
 
-# run tmux new -d if tmux daemon is not running
-if [[ $(tmux ls) ]]; then
-    tmux new -d
-fi
+# need to run tmux daemon
+tmux new -d
 tmux source-file ~/.tmux.conf
